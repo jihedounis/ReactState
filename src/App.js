0dc2dca9d126ea5a1770cbd760d022c359b14d7e
Jihed Ounis <tariqaq123@gmail.com>
2022-10-30 20:52:30 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Count from "./Component/count.js";
+class App extends Component {
+  state = {
+    fullName: 'Jihed Ounis',
+    bio: 'full stack web developer',
+    imgSrc: 'https://i.postimg.cc/cCcjT8V6/301238514-3299541836960843-1298940158327304747-n.jpg',
+    profession: 'Web Developer & Graphic designer',
+    show: false
+  }
+  handleShow = () => {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+  render() {
+    return (
+      <div className="Contain" >
+      <div className="App">
+        {this.state.show ? (
+          <>
+            <h1>{this.state.fullName}</h1>
+            <h1>{this.state.bio}</h1>
+            <img src={this.state.imgSrc} alt="failed" />
+            <h1>{this.state.profession}</h1>
+          </>
+        ) : (
+          <h1>Show the profile</h1>
+        )
+        }
+        <button onClick={this.handleShow}>{!this.state.show ? "show" : "hide"}</button>
+        <>
+        <h1>Show the counter </h1>
+        <Count />
+        </>
+      </div>
+      </div>
+    )
+    
+  };
 }
 
 export default App;
---
+++ src/Component/count.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+
+class Counter extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    Increment = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    Decrement = () => {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+    
+    componentDidMount() {
+        const interval = setInterval(() => {
+            this.setState({ count: this.state.count + 1 })
+        }, 1000)
+        
+        console.log("componentDidMount",interval);
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate")
+    }
+    componentWillUnmount() {
+        if (this.state.count < 10) {
+            clearInterval(this.interval);
+        }
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={() => this.Increment()}>+</button>
+                COUNT:  {this.state.count}
+                <button onClick={() => this.Decrement()}>-</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
